0dc91d4da71c0bb670635493ec5e89683151517b
Alexander Safronov <82944174+AlexanderGitSafronov@users.noreply.github.com>
2023-03-21 18:24:14 +0200

    simple calculator (#1)
    * simple calculator
    
    * calculator shorthand
    
    * delete return
    
    * delete return
    
    * add output
    
    * commit changes
    
    * editing
    
    * commit
    
    * commit

---
+++ js/app.js
@@ -0,0 +1,19 @@
+const firstNumber = document.querySelector(".first__number");
+const secondNumber = document.querySelector(".second__number");
+const result = document.querySelector(".result");
+const operation = document.querySelector(".operation");
+const resultOut = document.querySelector(".result__out");
+
+const OPERATIONS = {
+  ["+"]: (a, b) => a + b,
+  ["-"]: (a, b) => a - b,
+  ["*"]: (a, b) => a * b,
+  ["/"]: (a, b) => a / b,
+};
+
+result.addEventListener("click", () => {
+  resultOut.innerHTML = OPERATIONS[operation.value](
+    +firstNumber.value,
+    +secondNumber.value
+  );
+});
