f08855a37095e2e82cd4c9e13a1d4f6364ce0b99
Safronov Alexander <sasha.safronov1996161681@gmail.com>
2023-03-21 18:04:57 +0200

    add output

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
-const firstNumber = document.querySelector(".first__number");
-const secondNumber = document.querySelector(".second__number");
-const result = document.querySelector(".result");
-const operation = document.querySelector(".operation");
-const resultOut = document.querySelector(".result__out");
+const firstNumber = document.querySelector('.first__number');
+const secondNumber = document.querySelector('.second__number');
+const result = document.querySelector('.result')
+const operation = document.querySelector('.operation')
+const resultOut = document.querySelector('.result__out')
+
 
 const OPERATIONS = {
-  ["+"]: (a, b) => a + b,
-  ["-"]: (a, b) => a - b,
-  ["*"]: (a, b) => a * b,
-  ["/"]: (a, b) => a / b,
-};
+    ['+'] : (a,b) => a + b,
+    ['-'] : (a,b) => a - b,
+    ['*'] : (a,b) => a * b,
+    ['/'] : (a,b) => a / b
+}
 
-result.addEventListener("click", () => OPERATIONS[operation.value](firstNumber.value, secondNumber.value)
-);
+result.addEventListener('click', () => resultOut.innerHTML = OPERATIONS[operation.value](+firstNumber.value,+secondNumber.value)
+)
+
+
