2662cfb57965dd89a63b617b4f11e206f2274b37
Safronov Alexander <sasha.safronov1996161681@gmail.com>
2023-03-26 21:48:46 +0300

    added enumeration of all inputs and the ability to recalculate them

--- js/app.js
+++ js/app.js
@@ -1,19 +1,66 @@
-const firstNumber = document.querySelector(".first__number");
-const secondNumber = document.querySelector(".second__number");
 const result = document.querySelector(".result");
 const operation = document.querySelector(".operation");
 const resultOut = document.querySelector(".result__out");
 
-const OPERATIONS = {
-  ["+"]: (a, b) => a + b,
-  ["-"]: (a, b) => a - b,
-  ["*"]: (a, b) => a * b,
-  ["/"]: (a, b) => a / b,
-};
+const inputsBlock = document.querySelector(".inputs");
+const addInput = document.querySelector(".add__input");
+let inputs = Array.from(document.querySelectorAll(".input"));
+
+addInput.addEventListener("click", () => {
+  const newInput = `<input class="input" type="number" />`;
+  inputsBlock.insertAdjacentHTML("beforeend", newInput);
+  inputs = document.querySelectorAll(".input");
+});
 
 result.addEventListener("click", () => {
-  resultOut.innerHTML = OPERATIONS[operation.value](
-    +firstNumber.value,
-    +secondNumber.value
-  );
+  calculation(inputs);
 });
+
+function calculation(inputs) {
+  if (operation.value === "+") {
+    let resPlus = 0;
+    inputs.forEach((item) => {
+      resPlus += +item.value;
+    });
+    resultOut.innerHTML = resPlus;
+  }
+
+  if (operation.value === "-") {
+    let resSubtraction = 0;
+    inputs.forEach((item, idx) => {
+      if (idx === 0) {
+        resSubtraction = item.value;
+      }
+      if (idx > 0) {
+        resSubtraction -= item.value;
+      }
+    });
+    resultOut.innerHTML = resSubtraction;
+  }
+
+  if (operation.value === "*") {
+    let resSubtraction = 0;
+    inputs.forEach((item, idx) => {
+      if (idx === 0) {
+        resSubtraction = item.value;
+      }
+      if (idx > 0) {
+        resSubtraction *= item.value;
+      }
+    });
+    resultOut.innerHTML = resSubtraction;
+  }
+
+  if (operation.value === "/") {
+    let resSubtraction = 0;
+    inputs.forEach((item, idx) => {
+      if (idx === 0) {
+        resSubtraction = item.value;
+      }
+      if (idx > 0) {
+        resSubtraction /= item.value;
+      }
+    });
+    resultOut.innerHTML = resSubtraction;
+  }
+}
