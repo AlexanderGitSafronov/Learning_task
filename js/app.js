4446980104e8cad900f3d136306b392ec9e433a9
Safronov Alexander <sasha.safronov1996161681@gmail.com>
2023-03-31 17:59:55 +0300

    moved functions to the end of the document

--- js/app.js
+++ js/app.js
@@ -4,19 +4,32 @@ const resultOut = document.querySelector(".result__out");
 const inputsBlock = document.querySelector(".inputs");
 const addInput = document.querySelector(".add__input");
 
-function getInputs() {
-  return Array.from(document.querySelectorAll(".input"));
-}
-function getDelete() {
-  return Array.from(document.querySelectorAll(".delete__input"));
-}
-
+// Добаляєм поля Input
 addInput.addEventListener("click", () => {
   const newInput = `<div class="wrapper__input">
                         <input class="input" type="number" /><button class="delete__input">Delete</button>
                     </div>`;
   inputsBlock.insertAdjacentHTML("beforeend", newInput);
 });
+
+// Результат операції
+result.addEventListener("click", () => {
+  resultOut.innerHTML = reduceInputs(getInputs());
+});
+
+// Видалення Input
+inputsBlock.addEventListener("click", (e) => {
+  if (e.target.className === "delete__input") {
+    e.target.closest(".wrapper__input").remove();
+  }
+});
+
+function getInputs() {
+  return Array.from(document.querySelectorAll(".input"));
+}
+function getDelete() {
+  return Array.from(document.querySelectorAll(".delete__input"));
+}
 
 function reduceInputs(inputs) {
   let result = inputs.slice(1).reduce((acc, item) => {
@@ -28,14 +41,3 @@ function reduceInputs(inputs) {
   }, +inputs[0].value);
   return result;
 }
-
-result.addEventListener("click", () => {
-  resultOut.innerHTML = reduceInputs(getInputs());
-});
-
-// Видалення Input
-inputsBlock.addEventListener("click", (e) => {
-  if (e.target.className === "delete__input") {
-    e.target.closest(".wrapper__input").remove();
-  }
-});
