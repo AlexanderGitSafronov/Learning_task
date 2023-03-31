3650ab5737847f16ce5de44a974ad0b5730ef8e1
Alexander Safronov <82944174+AlexanderGitSafronov@users.noreply.github.com>
2023-03-31 18:11:43 +0300

    input_delete (#4)
    * input_delete
    
    * Added delete buttons and their functionality
    
    * catching clicks on dynamically changeable elements
    
    * removed wrapper function
    
    * moved functions to the end of the document

--- js/app.js
+++ js/app.js
@@ -1,18 +1,35 @@
 const result = document.querySelector(".result");
 const operation = document.querySelector(".operation");
 const resultOut = document.querySelector(".result__out");
-
 const inputsBlock = document.querySelector(".inputs");
 const addInput = document.querySelector(".add__input");
 
-function getInputs(){
-  return Array.from(document.querySelectorAll(".input"));
-}
-
+// Добаляєм поля Input
 addInput.addEventListener("click", () => {
-  const newInput = `<input class="input" type="number" />`;
+  const newInput = `<div class="wrapper__input">
+                        <input class="input" type="number" /><button class="delete__input">Delete</button>
+                    </div>`;
   inputsBlock.insertAdjacentHTML("beforeend", newInput);
 });
+
+// Результат операції
+result.addEventListener("click", () => {
+  resultOut.innerHTML = reduceInputs(getInputs());
+});
+
+// Видалення Input
+inputsBlock.addEventListener("click", (e) => {
+  if (e.target.className === "delete__input") {
+    e.target.closest(".wrapper__input").remove();
+  }
+});
+
+function getInputs() {
+  return Array.from(document.querySelectorAll(".input"));
+}
+function getDelete() {
+  return Array.from(document.querySelectorAll(".delete__input"));
+}
 
 function reduceInputs(inputs) {
   let result = inputs.slice(1).reduce((acc, item) => {
@@ -24,7 +41,3 @@ function reduceInputs(inputs) {
   }, +inputs[0].value);
   return result;
 }
-
-result.addEventListener("click", () => {
-  resultOut.innerHTML = reduceInputs(getinputs());
-});
