0f6fdde392009a9613d28ee6f39e107540091b45
Alexander Safronov <82944174+AlexanderGitSafronov@users.noreply.github.com>
2023-03-31 14:14:05 +0300

    add button add-input (#3)
    * add button add-input
    
    * added enumeration of all inputs and the ability to recalculate them
    
    * Did the job via Reduce

--- js/app.js
+++ js/app.js
@@ -1,19 +1,30 @@
-const firstNumber = document.querySelector(".first__number");
-const secondNumber = document.querySelector(".second__number");
 const result = document.querySelector(".result");
 const operation = document.querySelector(".operation");
 const resultOut = document.querySelector(".result__out");
 
-const OPERATIONS = {
-  ["+"]: (a, b) => a + b,
-  ["-"]: (a, b) => a - b,
-  ["*"]: (a, b) => a * b,
-  ["/"]: (a, b) => a / b,
-};
+const inputsBlock = document.querySelector(".inputs");
+const addInput = document.querySelector(".add__input");
+
+function getInputs(){
+  return Array.from(document.querySelectorAll(".input"));
+}
+
+addInput.addEventListener("click", () => {
+  const newInput = `<input class="input" type="number" />`;
+  inputsBlock.insertAdjacentHTML("beforeend", newInput);
+});
+
+function reduceInputs(inputs) {
+  let result = inputs.slice(1).reduce((acc, item) => {
+    operation.value === "+" ? (acc += +item.value) : "";
+    operation.value === "-" ? (acc -= +item.value) : "";
+    operation.value === "*" ? (acc *= +item.value) : "";
+    operation.value === "/" ? (acc /= +item.value) : "";
+    return acc;
+  }, +inputs[0].value);
+  return result;
+}
 
 result.addEventListener("click", () => {
-  resultOut.innerHTML = OPERATIONS[operation.value](
-    +firstNumber.value,
-    +secondNumber.value
-  );
+  resultOut.innerHTML = reduceInputs(getinputs());
 });
