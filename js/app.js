a2e977944affd7bbad821b19cc47e65f1d62786d
Safronov Alexander <sasha.safronov1996161681@gmail.com>
2023-03-31 15:34:15 +0300

    Added delete buttons and their functionality

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
 const result = document.querySelector(".result");
 const operation = document.querySelector(".operation");
 const resultOut = document.querySelector(".result__out");
-
 const inputsBlock = document.querySelector(".inputs");
 const addInput = document.querySelector(".add__input");
 
-function getInputs(){
+function getInputs() {
   return Array.from(document.querySelectorAll(".input"));
+}
+function getDelete() {
+  return Array.from(document.querySelectorAll(".delete__input"));
 }
 
 addInput.addEventListener("click", () => {
-  const newInput = `<input class="input" type="number" />`;
+  const newInput = `<div class="wrapper__input">
+                        <input class="input" type="number" /><button class="delete__input">Delete</button>
+                    </div>`;
   inputsBlock.insertAdjacentHTML("beforeend", newInput);
+  deleteInput();
 });
 
 function reduceInputs(inputs) {
@@ -29,3 +34,12 @@ result.addEventListener("click", () => {
   resultOut.innerHTML = reduceInputs(getinputs());
 });
 
+// Видалення Input
+function deleteInput() {
+  getDelete().forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.target.closest(".wrapper__input").remove();
+    });
+  });
+}
+deleteInput();
