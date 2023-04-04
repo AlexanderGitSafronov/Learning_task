1299ee9dcef17a95c4ae2942b413e286d7273a10
Safronov Alexander <sasha.safronov1996161681@gmail.com>
2023-04-04 18:35:50 +0300

    implemented the maximum number of 5 inputs

--- js/app.js
+++ js/app.js
@@ -4,15 +4,26 @@ const resultOut = document.querySelector(".result__out");
 const inputsBlock = document.querySelector(".inputs");
 const addInput = document.querySelector(".add__input");
 const error = document.querySelector(".error");
+let wrapperInput = document.querySelectorAll(".wrapper__input");
 
+const errorMessage = `<div class="error">
+        <p class="error__message">Не можна видалити 2 останні інпути</p>
+      </div>`;
 // Добаляєм поля Input
 addInput.addEventListener("click", () => {
   const newInput = `<div class="wrapper__input">
-                        <input class="input" type="number" /><button class="delete__input">Delete</button>
-                    </div>`;
-  inputsBlock.insertAdjacentHTML("beforeend", newInput);
-
-  error.style.display = "none";
+  <input class="input" type="number" /><button class="delete__input">Delete</button>
+  </div>`;
+  setTimeout(() => {
+    if (getInputs().length < 5) {
+      if (lastChildren().classList.contains("error")) {
+        lastChildren().remove();
+      }
+      inputsBlock.insertAdjacentHTML("beforeend", newInput);
+    } else {
+      errorCount();
+    }
+  }, 100);
 });
 
 // Результат операції
@@ -23,12 +34,21 @@ result.addEventListener("click", () => {
 // Взаємодія з кнопкою видалення
 inputsBlock.addEventListener("click", (e) => {
   if (e.target.className === "delete__input") {
-    if (getInputs().length <= 2) {
-      error.style.display = "block";
-    }
-    if (getInputs().length > 2) {
-      e.target.closest(".wrapper__input").remove();
-    }
+    setTimeout(() => {
+      if (getInputs().length <= 2) {
+        if (!lastChildren().classList.contains("error")) {
+          inputsBlock.insertAdjacentHTML("beforeend", errorMessage);
+        }
+      }
+    }, 100);
+    setTimeout(() => {
+      if (getInputs().length > 2) {
+        e.target.closest(".wrapper__input").remove();
+        if (lastChildren().classList.contains("error")) {
+          lastChildren().remove();
+        }
+      }
+    }, 100);
   }
 });
 
@@ -38,6 +58,10 @@ function getInputs() {
 function getDelete() {
   return Array.from(document.querySelectorAll(".delete__input"));
 }
+function lastChildren() {
+  return inputsBlock.lastElementChild;
+}
+
 function reduceInputs(inputs) {
   let result = inputs.slice(1).reduce((acc, item) => {
     operation.value === "+" ? (acc += +item.value) : "";
@@ -47,4 +71,10 @@ function reduceInputs(inputs) {
     return acc;
   }, +inputs[0].value);
   return result;
+}
+
+function errorCount() {
+  if (!lastChildren().classList.contains("error")) {
+    inputsBlock.insertAdjacentHTML("beforeend", errorMessage);
+  }
 }
